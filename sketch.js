09fea9b411ab851351c9b1d9b354fb76b491e76e
soonitoon <soonitoon@gmail.com>
2021-05-17 08:24:38 +0900

    Import poseNet

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
 let video;
+let poseNet;
 
 function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+}
+
+function modelLoaded() {
+  console.log("poseNet ready");
 }
 
 function draw() {
