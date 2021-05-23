68618679b32a0749f4626f6030b311d0336c487b
soonitoon <soonitoon@gmail.com>
2021-05-24 02:13:59 +0900

    Feat: Train model

--- sketch.js
+++ sketch.js
@@ -39,7 +39,17 @@ function setup() {
     debug: true,
   };
   brain = ml5.neuralNetwork(options);
-  brain.loadData("collected.json");
+  brain.loadData("collected.json", dataReady);
+}
+
+function dataReady() {
+  brain.normalizeData();
+  brain.train({ epochs: 50 }, finished);
+}
+
+function finished() {
+  console.log("model trained");
+  brain.save();
 }
 
 function gotPoses(poses) {
