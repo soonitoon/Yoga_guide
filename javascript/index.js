0d8f0143a518bcf3ce029c26fffc991a46e94273
soonitoon <soonitoon@gmail.com>
2021-05-28 01:34:15 +0900

    Feat: Make prograss bar move

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,20 @@
-setTimeout(() => {
-  location.replace("html/main.html");
-}, 1500);
+// setTimeout(() => {
+//   location.replace("html/main.html");
+// }, 1500);
+
+function moveProgressBar() {
+  const elem = document.querySelector(".progress_bar");
+  let width = 20;
+  const id = setInterval(frame, 15);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + "%";
+      elem.innerHTML = width * 1 + "%";
+    }
+  }
+}
+
+moveProgressBar();
