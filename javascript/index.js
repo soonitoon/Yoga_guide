74a6479c2dc2476ac9a32febd4e3137dead56d98
soonitoon <soonitoon@gmail.com>
2021-05-28 02:23:20 +0900

    Refactor: Make loading time as global variable

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,13 @@
-// setTimeout(() => {
-//   location.replace("html/main.html");
-// }, 1500);
+const LOADING_TIMER = 1.5; // sec
+
+setTimeout(() => {
+  location.replace("html/main.html");
+}, LOADING_TIMER * 1000);
 
 function moveProgressBar() {
   const elem = document.querySelector(".progress_bar");
   let width = 20;
-  const id = setInterval(frame, 15);
+  const id = setInterval(frame, LOADING_TIMER * 10);
   function frame() {
     if (width >= 100) {
       clearInterval(id);
