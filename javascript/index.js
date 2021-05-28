a839ec54fbb2c7dec8a1d2c4c50d567e26c6c1e7
soonitoon <soonitoon@gmail.com>
2021-05-29 01:51:13 +0900

    Delete progress bar

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,5 @@
 const LOADING_TIMER = 1.5; // sec
 
-setTimeout(() => {
-  location.replace("html/main.html");
-}, LOADING_TIMER * 1000);
-
-function moveProgressBar() {
-  const elem = document.querySelector(".progress_bar");
-  let width = 20;
-  const id = setInterval(frame, LOADING_TIMER * 10);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width++;
-      elem.style.width = width + "%";
-      elem.innerHTML = width * 1 + "%";
-    }
-  }
-}
-
-moveProgressBar();
+// setTimeout(() => {
+//   location.replace("html/main.html");
+// }, LOADING_TIMER * 1000);
